9e6db544c771027ac3ed342e26d6a2840881caa6
fiskerton-45 <capyhalo@gmail.com>
2022-12-15 13:22:53 -0600

    creacion base del proyecto

--- app/src/main/java/com/registro/MainActivity.kt
+++ app/src/main/java/com/registro/MainActivity.kt
@@ -2,10 +2,29 @@ package com.registro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(applicationContext, "gg", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+            }
     }
 }
